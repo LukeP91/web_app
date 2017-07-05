b96d9c64389edaf00f933c8f1712710bf8704b86
Luke Pawlik <lukepawlik@gmail.com>
2017-07-05 22:05:56 +0200

    user can now edit his profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,21 @@
 class ProfilesController < ApplicationController
   def show
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      redirect_to profile_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :age, :gender)
+  end
 end
