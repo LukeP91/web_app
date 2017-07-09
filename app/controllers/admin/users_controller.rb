248d5d15a01d9511922077e200963dbf3fd40fd1
Luke Pawlik <lukepawlik@gmail.com>
2017-07-09 17:47:01 +0200

    move dashboard to Admin::UsersController. add link to admin panel which is visible only for admin ProfileController#Show is now root of web app

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,7 @@
-class DashboardController < ApplicationController
+class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
     @users = User.all
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
 
-  root to: 'dashboard#index'
+  root to: 'profiles#show'
   resource :profile, only: [:show, :edit, :update]
+
+  namespace :admin do
+    resources :users, only: [:index]
+  end
 end
