be231df9598033b9ae5128b9ab71b3d6bcafcb1f
Luke Pawlik <lukepawlik@gmail.com>
2017-07-09 19:37:28 +0200

    admin can now edit other users profiles

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,39 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
   def index
     @users = User.all
     authorize @users
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update_attributes(user_params)
+      redirect_to admin_user_path
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete(:password_confirmation)
+      params[:user].delete(:password)
+    end
+    params.require(:user).permit(:email, :first_name, :last_name, :age, :gender,
+                                 :admin, :password, :password_confirmation)
   end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,5 +1,17 @@
 class UserPolicy < ApplicationPolicy
+  def show?
+    user.admin?
+  end
+
   def index?
+    user.admin?
+  end
+
+  def edit?
+    user.admin?
+  end
+
+  def update?
     user.admin?
   end
 end
