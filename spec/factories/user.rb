3698b05b387ef6a63b374557fd944128ea46f58e
Luke Pawlik <lukepawlik@gmail.com>
2017-07-16 20:23:29 +0200

    * add factory girl to project and create factiories for user and interest * add seeds to project using factories

---
+++ spec/factories/user.rb
@@ -0,0 +1,45 @@
+FactoryGirl.define do
+  factory :user do
+    first_name { FFaker::Name.first_name }
+    last_name  { FFaker::Name.last_name }
+    sequence(:email) { |n| "#{first_name}.#{last_name}_#{n}@example.com".downcase }
+    admin false
+    password "password"
+    password_confirmation "password"
+
+    factory :admin do
+      email "admin@example.com"
+      admin true
+    end
+
+    trait :female do
+      gender "female"
+    end
+
+    trait :male do
+      gender "male"
+    end
+
+    trait :older_than_30 do
+      age { Random.new.rand(31..100) }
+    end
+
+    trait :younger_than_20 do
+      age { Random.new.rand(2..19) }
+    end
+
+    trait :between_20_and_30 do
+      age { Random.new.rand(20..30) }
+    end
+
+    factory :user_with_interests do
+      transient do
+        interests_count 5
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:interest, evaluator.interests_count, user: user)
+      end
+    end
+  end
+end
