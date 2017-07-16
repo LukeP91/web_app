6baf5aab8860c7730e5f19a96ca5dc1ca6d10fa3
Luke Pawlik <lukepawlik@gmail.com>
2017-07-16 12:43:26 +0200

    add search by email to index page

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require_relative 'boot'
 
 require 'csv'
+require File.expand_path('../boot', __FILE__)
+ENV['RANSACK_FORM_BUILDER'] = '::SimpleForm::FormBuilder'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
