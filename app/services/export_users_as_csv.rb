1a70572c293fb1e465cf383e98a574630522a8fb
Luke Pawlik <lukepawlik@gmail.com>
2017-07-22 17:42:59 +0200

    fix minor styling issue in export service

--- app/services/export_users_as_csv.rb
+++ app/services/export_users_as_csv.rb
@@ -1,5 +1,5 @@
 class ExportUsersAsCSV
   def self.export(users)
-    CSV.generate { |csv| users.map{ |user| csv << user.values_to_export } }
+    CSV.generate { |csv| users.map { |user| csv << user.values_to_export } }
   end
 end
