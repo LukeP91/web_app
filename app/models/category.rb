4d58e978d1e25ba0c37364e32b0d99860117ba6f
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-02 09:10:20 +0200

    category and interest belong to only one organization
    * fix migrations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   has_many :interests, through: :users_interests
   has_many :users_interests
+  belongs_to :organization
 
+  scope :from_organization, ->(organization_id) { where(organization_id: organization_id) }
   validates :name, presence: true
 end
