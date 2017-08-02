b5302e84c7dcd426e1ae09eafa872aa48f7e74dc
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-02 12:12:35 +0200

    move scope below validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ApplicationRecord
   has_many :users_interests
   belongs_to :organization
 
-  scope :from_organization, ->(organization_id) { where(organization_id: organization_id) }
   validates :name, presence: true
+
+  scope :from_organization, ->(organization_id) { where(organization_id: organization_id) }
 end
