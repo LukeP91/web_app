c80f2d84841e5844da70cd037ae8c2f4a954ef3c
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-02 11:11:38 +0200

    fix redirect_to resource

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -44,7 +44,7 @@ class Admin::UsersController < ApplicationController
     user = User.find(params[:id])
     authorize user
     if user.update(user_params)
-      redirect_to admin_user_path
+      redirect_to admin_user_path(user)
     else
       render :edit
     end
