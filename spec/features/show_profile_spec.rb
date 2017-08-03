828d18995725aca232ad41f8d59a4782ca24e111
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-03 13:01:39 +0200

    refactor profile tests to use site prism

--- spec/features/show_profile_spec.rb
+++ spec/features/show_profile_spec.rb
@@ -1,44 +1,50 @@
 require 'rails_helper'
-require 'support/pages/home'
-require 'support/pages/login_page'
+require 'support/pages/app'
 
 describe 'Show profile', type: :feature do
   let(:user) { create(:user) }
   let(:complete_user) { create(:user_with_interests, :male, :older_than_30) }
 
-  scenario 'Signed in user can see his profile' do
-    @home_page = Home.new
-    @home_page.load
+  context 'signed in user' do
+    scenario 'with mandatory data can see basic info on profile' do
+      app = App.new
+      app.home_page.load
 
-    @login_page = LoginPage.new
-    @login_page.email_field.set user.email
-    @login_page.password_field.set user.password
-    @login_page.login_button.click
+      app.login_page.email_field.set user.email
+      app.login_page.password_field.set user.password
+      app.login_page.login_button.click
 
-    expect(page).to have_css('h1', text: user.full_name)
+      expect(app.home_page.text).to include user.full_name
+      expect(app.home_page.text).to include user.email
+      expect(app.home_page.text).to include user.first_name
+      expect(app.home_page.text).to include user.last_name
+      expect(app.home_page.text).to_not include 'Gender'
+      expect(app.home_page.text).to_not include 'Age'
+    end
+
+    scenario 'with complete data can see all his info on profile' do
+      app = App.new
+      app.home_page.load
+
+      app.login_page.email_field.set complete_user.email
+      app.login_page.password_field.set complete_user.password
+      app.login_page.login_button.click
+
+      expect(app.home_page.text).to include complete_user.full_name
+      expect(app.home_page.text).to include complete_user.email
+      expect(app.home_page.text).to include complete_user.first_name
+      expect(app.home_page.text).to include complete_user.last_name
+      expect(app.home_page.text).to include complete_user.gender
+      expect(app.home_page.text).to include complete_user.age.to_s
+    end
   end
 
-  scenario 'User with complete data can see all his info on profile' do
-    @home_page = Home.new
-    @home_page.load
+  context 'not signed in user' do
+    scenario "can't see his profile" do
+      app = App.new
+      app.home_page.load
 
-    @login_page = LoginPage.new
-    @login_page.email_field.set complete_user.email
-    @login_page.password_field.set complete_user.password
-    @login_page.login_button.click
-
-    expect(page).to have_css('h1', text: complete_user.full_name)
-    expect(page).to have_css('p', text: complete_user.email)
-    expect(page).to have_css('p', text: complete_user.first_name)
-    expect(page).to have_css('p', text: complete_user.last_name)
-    expect(page).to have_css('p', text: complete_user.gender)
-    expect(page).to have_css('p', text: complete_user.age)
-  end
-
-  scenario "Not signed in user can't see his profile" do
-    @home_page = Home.new
-    @home_page.load
-
-    expect(page).to have_content 'Log in'
+      expect(page).to have_content 'Log in'
+    end
   end
 end
--- spec/support/pages/home.rb
+++ spec/support/pages/home.rb
@@ -1,3 +1,7 @@
 class Home < SitePrism::Page
   set_url '/'
+
+  def find_fullname(full_name)
+    page.find(:xpath, "(//h1[contains(text(), '#{full_name}')])")
+  end
 end
