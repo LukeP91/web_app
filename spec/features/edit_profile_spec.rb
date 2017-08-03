6c6674e6feb088f9d00cb6c66c99e2cbf678ba0f
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-03 13:56:59 +0200

    refactor edit_profile_spec.rb

--- spec/features/edit_profile_spec.rb
+++ spec/features/edit_profile_spec.rb
@@ -1,61 +1,75 @@
-require "rails_helper"
+require 'rails_helper'
 
-describe "Edit profile", type: :feature do
-  before :each do
-    @user = create(:user, :male, :older_than_30)
+describe 'Edit profile', type: :feature do
+  context 'signed in user' do
+    before :each do
+      @user = create(:user, :male, :older_than_30)
+    end
+
+    let(:modified_user) do
+      { email: 'changed_email@example.com',
+        first_name: 'Modified',
+        last_name: 'Modified',
+        gender: 'female',
+        age: 15 }
+    end
+
+    scenario 'can edit his profile' do
+      app = App.new
+      app.home_page.load
+
+      app.login_page.email_field.set @user.email
+      app.login_page.password_field.set @user.password
+      app.login_page.login_button.click
+
+      app.home_page.edit_button.click
+      expect(app.edit_profile_page).to be_displayed
+
+      app.edit_profile_page.email_field.set modified_user[:email]
+      app.edit_profile_page.first_name_field.set modified_user[:first_name]
+      app.edit_profile_page.last_name_field.set modified_user[:last_name]
+      app.edit_profile_page.gender_field.select modified_user[:gender]
+      app.edit_profile_page.age_field.select modified_user[:age]
+      app.edit_profile_page.update_button.click
+
+      expect(app.home_page.text).to include "#{modified_user[:first_name]} #{modified_user[:last_name]}"
+      expect(app.home_page.text).to include modified_user[:email]
+      expect(app.home_page.text).to include modified_user[:first_name]
+      expect(app.home_page.text).to include modified_user[:last_name]
+      expect(app.home_page.text).to include modified_user[:gender]
+      expect(app.home_page.text).to include modified_user[:age].to_s
+    end
+
+    scenario 'can select interests' do
+      create(:interest, name: 'test1')
+      create(:interest, name: 'test2')
+      create(:interest, name: 'test3')
+
+      app = App.new
+      app.home_page.load
+
+      app.login_page.email_field.set @user.email
+      app.login_page.password_field.set @user.password
+      app.login_page.login_button.click
+
+      app.home_page.edit_button.click
+      expect(app.edit_profile_page).to be_displayed
+
+      app.edit_profile_page.interests_field.select 'test1'
+      app.edit_profile_page.interests_field.select 'test2'
+      app.edit_profile_page.update_button.click
+
+      expect(app.home_page.text).to include('test1', 'test2')
+      expect(app.home_page.text).to_not include 'test3'
+    end
   end
 
-  let(:modified_user) { { email: "changed_email@example.com",
-                          first_name: "Modified",
-                          last_name: "Modified",
-                          gender: "female",
-                          age: 15 } }
+  context 'Not signed in user' do
+    scenario "Not signed in user can't edit his profile" do
+      app = App.new
+      app.home_page.load
 
-  scenario "Signed in user can edit his profile" do
-    visit "/"
-    within("#new_user") do
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
+      expect(app.login_page.text).to have_content 'Log in'
     end
-    click_button 'Log in'
-    click_link 'Edit'
-    expect(page).to have_css("h1", text: "Edit User")
-
-    fill_in 'Email', with: modified_user[:email]
-    fill_in 'First name', with: modified_user[:first_name]
-    fill_in 'Last name', with: modified_user[:last_name]
-    select modified_user[:gender], from: 'Gender'
-    select modified_user[:age], from: 'Age'
-    click_button 'Update User'
-
-    expect(page).to have_css("h1", text: "#{modified_user[:first_name]} #{modified_user[:last_name] }")
-    expect(page).to have_css("p", text: modified_user[:email])
-    expect(page).to have_css("p", text: modified_user[:first_name])
-    expect(page).to have_css("p", text: modified_user[:last_name])
-    expect(page).to have_css("p", text: modified_user[:gender])
-    expect(page).to have_css("p", text: modified_user[:age])
-  end
-
-  scenario "Signed in user can add interest to his profile" do
-    visit "/"
-    within("#new_user") do
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-    end
-    click_button 'Log in'
-    click_link 'Edit'
-    expect(page).to have_css("h1", text: "Edit User")
-    click_link 'add interest'
-    # add sleep to wait for name element
-    within('#interests') do
-      fill_in 'Name', with: "Interest"
-      select 'health', from: 'Category'
-    end
-    click_button 'Update User'
-  end
-
-  scenario "Not signed in user can't edit his profile" do
-    visit "/profile/edit"
-    expect(page).to have_content "Log in"
   end
 end
