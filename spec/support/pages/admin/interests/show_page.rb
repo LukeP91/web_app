18cb27d4feb08b0a35f6ad0f9b145dcd3160aca1
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-04 14:47:44 +0200

    add admin interests index specs

---
+++ spec/support/pages/admin/interests/show_page.rb
@@ -0,0 +1,7 @@
+module Admin
+  module Interests
+    class ShowPage < SitePrism::Page
+      set_url '/admin/interests/{id}'
+    end
+  end
+end
--- spec/support/pages/sections/menu.rb
+++ spec/support/pages/sections/menu.rb
@@ -1,6 +1,7 @@
 module Sections
   class Menu < SitePrism::Section
-    element :admin_categories_link, "a[href='/admin/categories']"
     element :admin_panel_link, "a[href='/admin/users']"
+    element :admin_categories_link, "a[href='/admin/categories']"
+    element :admin_interests_link, "a[href='/admin/interests']"
   end
 end
