b8ed543feeea39bcca970bd8e8fc9f507cf54976
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-04 15:12:47 +0200

    add ids to fields used in tests
    * changed all xpath finders to css selectors

--- spec/support/pages/profile/home.rb
+++ spec/support/pages/profile/home.rb
@@ -3,10 +3,10 @@ module Profile
     section :menu, Sections::Menu, '.navbar'
     set_url '/'
 
-    element :edit_button, "a[href='/profile/edit']"
+    element :edit_button, "#edit_button"
 
     def find_fullname(full_name)
-      page.find(:xpath, "(//h1[contains(text(), '#{full_name}')])")
+      page.find(:xpath, "#full_name")
     end
   end
 end
