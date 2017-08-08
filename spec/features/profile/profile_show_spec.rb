75c3adda20a185be14103c25ef7d28959c2c1a32
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-08 14:13:08 +0200

    refactor profile tests

--- spec/features/profile/profile_show_spec.rb
+++ spec/features/profile/profile_show_spec.rb
@@ -3,37 +3,19 @@ require 'support/pages/app'
 
 describe 'Show profile' do
   context 'signed in user' do
-    before do
-      @user = create(:user)
-      @complete_user = create(:user_with_interests, :male, :older_than_30)
-    end
-
-    scenario 'with mandatory data can see basic info on profile' do
-      app = App.new
-      app.home_page.load
-      app.login_page.login(@user)
-      expect(app.home_page).to be_displayed
-
-      expect(app.home_page.text).to include @user.full_name
-      expect(app.home_page.text).to include @user.email
-      expect(app.home_page.text).to include @user.first_name
-      expect(app.home_page.text).to include @user.last_name
-      expect(app.home_page.text).to_not include 'Gender'
-      expect(app.home_page.text).to_not include 'Age'
-    end
-
     scenario 'with complete data can see all his info on profile' do
+      user = create(:user_with_interests, :male, email: 'joe.doe@example.com', first_name: 'Joe', last_name: 'Doe', age: 25)
       app = App.new
       app.home_page.load
-      app.login_page.login(@complete_user)
+      app.login_page.login(user)
       expect(app.home_page).to be_displayed
 
-      expect(app.home_page.text).to include @complete_user.full_name
-      expect(app.home_page.text).to include @complete_user.email
-      expect(app.home_page.text).to include @complete_user.first_name
-      expect(app.home_page.text).to include @complete_user.last_name
-      expect(app.home_page.text).to include @complete_user.gender
-      expect(app.home_page.text).to include @complete_user.age.to_s
+      expect(app.home_page.full_name_field.text).to eq 'Joe Doe'
+      expect(app.home_page.field_by_label('Email').text).to eq 'Email: joe.doe@example.com'
+      expect(app.home_page.field_by_label('First name').text).to eq 'First name: Joe'
+      expect(app.home_page.field_by_label('Last name').text).to eq 'Last name: Doe'
+      expect(app.home_page.field_by_label('Gender').text).to eq 'Gender: male'
+      expect(app.home_page.field_by_label('Age').text).to eq 'Age: 25'
     end
   end
 
