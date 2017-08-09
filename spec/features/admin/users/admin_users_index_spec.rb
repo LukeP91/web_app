10fa1f4839995eca078651eaba91445d93a895cc
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-09 09:18:21 +0200

    remove unnecessary spec

--- spec/features/admin/users/admin_users_index_spec.rb
+++ spec/features/admin/users/admin_users_index_spec.rb
@@ -2,18 +2,6 @@ require 'rails_helper'
 
 describe 'Admin listing' do
   context 'user with admin privileges' do
-    scenario 'can access dashboard' do
-      admin = create(:admin)
-
-      app = App.new
-      app.home_page.load
-      app.login_page.login(admin)
-      expect(app.home_page).to be_displayed
-
-      app.home_page.menu.admin_panel_link.click
-      expect(app.admin_users_index_page).to be_displayed
-    end
-
     scenario 'can see only users from his organization' do
       organization = create(:organization)
       admin = create(:admin, first_name: 'Luke', last_name: 'Admin', organization: organization)
