dfd9ef7baf1691b65c3ee76e8ebfb336135a72b7
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-11 15:48:43 +0200

    refactor service objects, so each has only one public method call

--- app/services/export_users_as_csv.rb
+++ app/services/export_users_as_csv.rb
@@ -1,5 +1,13 @@
 class ExportUsersAsCSV
-  def self.export(users)
+  def initialize(users)
+    @users = users
+  end
+
+  def call
     CSV.generate { |csv| users.map { |user| csv << user.values_to_export } }
   end
+
+  private
+
+  attr_reader :users
 end
