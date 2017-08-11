a4fe20627a48448f7ec4508fac83b87f266d7f31
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-11 15:47:56 +0200

    add test for export users as csv service

---
+++ spec/services/export_user_as_csv_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe ExportUsersAsCSV do
+  it 'generates csv with users' do
+    reading = create(:interest, name: 'Reading')
+    horse_riding = create(:interest, name: 'Horse Riding')
+    user = create(:user, first_name: 'Joe', last_name: 'Doe', email: 'joe.doe@example.com', gender: 'male', age: 34, interests: [reading, horse_riding])
+
+    test = ExportUsersAsCSV.new([user]).call
+
+    expect(test).to eq "Joe,Doe,joe.doe@example.com,male,34,Reading,Horse Riding\n"
+  end
+end
