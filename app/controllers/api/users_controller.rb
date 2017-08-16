be5f5eac048038ef802e454e1d459f585473f1f9
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-16 10:59:08 +0200

    add creating user via json api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,11 +1,34 @@
 class Api::UsersController < ApplicationController
   def index
     users = User.all
-    render json: users
+    render json: users, status: :ok
   end
 
   def show
     user = User.find(params[:id])
-    render json: user
+    render json: user, status: :ok
+  end
+
+  def create
+    user = User.new(user_params)
+    user.password = user.password_confirmation = Devise.friendly_token.first(8)
+    user.organization = Organization.first
+    if user.save
+      render json: user, status: :created
+    end
+  end
+
+  private
+
+  def user_params
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete(:password_confirmation)
+      params[:user].delete(:password)
+    end
+
+    params.require(:user).permit(
+      :email, :first_name, :last_name, :age, :gender, :admin, :password,
+      :password_confirmation, interest_ids: []
+    )
   end
 end
