6b9975d9badaea5e7cfd41973b8bc33191ea0d2a
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-16 13:35:03 +0200

    custom serializer for user show

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,2 +1,31 @@
 class UserSerializer
+  require 'json'
+
+  def initialize(resource)
+    @resource = resource
+  end
+
+  def serialize
+    JSON.generate(jsonapi_hash)
+  end
+
+  private
+
+  def jsonapi_hash
+    { data: resource_hash }
+  end
+
+  def resource_hash
+    { id: @resource.id, attributes: attributes_hash }
+  end
+
+  def attributes_hash
+    {
+      first_name: @resource.first_name,
+      last_name: @resource.last_name,
+      email: @resource.email,
+      age: @resource.age,
+      gender: @resource.gender
+    }
+  end
 end
