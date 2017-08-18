86cd3aba423aef1cecc4ba1924e66e4368978805
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-18 11:09:52 +0200

    apply feedback from stevo

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Api::UsersController do
   describe '#index' do
-    context 'authorized user' do
+    context 'when user is authorized' do
       it 'returns all users from organization' do
         organization = create(:organization)
         joe = create(
@@ -23,6 +23,15 @@ describe Api::UsersController do
           age: 60,
           gender: 'male',
           organization: organization
+        )
+
+        create(
+          :user,
+          first_name: 'Dave',
+          last_name: 'Joe',
+          email: 'dave.joe@example.com',
+          age: 60,
+          gender: 'male'
         )
 
         get :index
@@ -64,26 +73,29 @@ describe Api::UsersController do
 
       it 'returns paginated users with proper links' do
         organization = create(:organization)
-        create_list(:user, 2)
+        create_list(:user, 2, organization: organization)
         alice = create(
           :user,
           first_name: 'Alice',
           last_name: 'Cooper',
           email: 'alice.cooper@example.com',
           age: 60,
-          gender: 'male'
+          gender: 'male',
+          organization: organization
         )
-        create_list(:user, 2)
+        create_list(:user, 2, organization: organization)
 
         get :index, params: { page: 3, per_page: 1 }
 
         expect(response).to have_http_status(:ok)
-        expect(response.header['X-Total-Count']).to eq 5
-        expect(response.header['Link']).to include "<http://test.host/admin/users?page=1&per_page=1>; rel=\"first\""
-        expect(response.header['Link']).to include "<http://test.host/admin/users?page=5&per_page=1>; rel=\"last\""
-        expect(response.header['Link']).to include "<http://test.host/admin/users?page=2&per_page=1>; rel=\"prev\""
-        expect(response.header['Link']).to include "<http://test.host/admin/users?page=4&per_page=1>; rel=\"next\""
         expect(response.body).to include_json(
+          links: {
+            self: 'http://test.host/admin/users',
+            first: 'http://test.host/admin/users?page=1&per_page=1',
+            prev: 'http://test.host/admin/users?page=2&per_page=1',
+            next: 'http://test.host/admin/users?page=4&per_page=1',
+            last: 'http://test.host/admin/users?page=5&per_page=1'
+          },
           data: [
             {
               id: alice.id,
@@ -103,62 +115,68 @@ describe Api::UsersController do
         )
       end
 
-      it "returns empty 'Link' response header when there is only one page" do
-        get :index
+      describe 'links section' do
+        context 'where there is only one page' do
+          it 'returns only self' do
+            get :index
 
-        expect(response.header['Link']).to eq ''
-      end
+            expect(response.body).to include_json(links: { self: 'http://test.host/admin/users' })
+          end
+        end
 
-      it "doesn't return link to first page if already on it" do
-        create_list(:user, 2)
+        context 'when currently on first page' do
+          it "doesn't return link to first page" do
+            create_list(:user, 2)
 
-        get :index, params: {page: 1, per_page: 1}
+            get :index, params: { page: 1, per_page: 1 }
 
-        expect(response.header['Link']).to_not include "<http://test.host/admin/users?page=1&per_page=1>; rel=\"first\""
-      end
+            expect(response.body).to_not include_json(links: { first: 'http://test.host/admin/users?page=1&per_page=1' })
+          end
 
-      it "doesn't return link to last page if already on it" do
-        create_list(:user, 2)
+          it "doesn't return link to previous page" do
+            create_list(:user, 2)
 
-        get :index, params: {page: 2, per_page: 1}
+            get :index, params: { page: 1, per_page: 1 }
 
-        expect(response.header['Link']).to_not include "<http://test.host/admin/users?page=2&per_page=1>; rel=\"last\""
-      end
+            expect(response.body).to_not include_json(links: { prev: 'http://test.host/admin/users?page=1&per_page=1' })
+          end
+        end
 
-      it "doesn't return link to previous page if currently on first page" do
-        create_list(:user, 2)
+        context 'when currently on last page' do
+          it "doesn't return link to last page" do
+            create_list(:user, 2)
 
-        get :index, params: {page: 1, per_page: 1}
+            get :index, params: { page: 2, per_page: 1 }
 
-        expect(response.header['Link']).to_not include "<http://test.host/admin/users?page=1&per_page=1>; rel=\"prev\""
-      end
+            expect(response.body).to_not include_json(links: { last: 'http://test.host/admin/users?page=2&per_page=1' })
+          end
 
-      it "doesn't return link to next page if currently on last page" do
-        create_list(:user, 2)
+          it "doesn't return link to next page" do
+            create_list(:user, 2)
 
-        get :index, params: {page: 2, per_page: 1}
+            get :index, params: { page: 2, per_page: 1 }
 
-        expect(response.header['Link']).to_not include "<http://test.host/admin/users?page=2&per_page=1>; rel=\"next\""
+            expect(response.body).to_not include_json(links: { next: 'http://test.host/admin/users?page=2&per_page=1' })
+          end
+        end
       end
     end
 
-    context 'unauthorized user' do
+    context 'when user is unauthorized' do
+      xit 'returns unauthorized'
     end
   end
 
   describe '#show' do
-    context 'authorized user' do
+    context 'when user is authorized' do
       it 'returns user data in proper format' do
-        interest = create(:interest)
-
         user = create(
           :user,
           first_name: 'Joe',
           last_name: 'Doe',
           email: 'joe.doe@example.com',
           age: 50,
-          gender: 'male',
-          interests: [interest]
+          gender: 'male'
         )
 
         get :show, params: { id: user.id }
@@ -182,43 +200,52 @@ describe Api::UsersController do
         )
       end
 
-      it 'returns not found when user with given id does not exist in the DB' do
-        get :show, params: { id: 1 }
+      context 'when user has some interests' do
+        xit 'returnes user with his interests'
+      end
 
-        expect(response).to have_http_status(:not_found)
-        expect(response.body).to include_json(
-          errors: [
-            {
-              status: 404,
-              code: 'Not found',
-              title: 'User not found'
-            }
-          ]
-        )
+      context "when user with given id doesn't exist in the DB" do
+        it 'returns not found when user' do
+          get :show, params: { id: 1 }
+
+          expect(response).to have_http_status(:not_found)
+          expect(response.body).to include_json(
+            errors: [
+              {
+                status: 404,
+                code: 'Not found',
+                title: 'User not found'
+              }
+            ]
+          )
+        end
       end
     end
 
-    context 'unauthorized user' do
+    context 'when user is unauthorized' do
+      xit 'returns unauthorized'
     end
   end
 
   describe '#create' do
-    context 'authorized user' do
+    context 'when user is authorized' do
       it 'creates new user' do
         create(:organization)
 
-        post :create, params: {
-          data: {
-            type: 'users',
-            attributes: {
-              first_name: 'Joe',
-              last_name: 'Doe',
-              email: 'joe.doe@example.com',
-              age: 25,
-              gender: 'male'
+        expect do
+          post :create, params: {
+            data: {
+              type: 'users',
+              attributes: {
+                first_name: 'Joe',
+                last_name: 'Doe',
+                email: 'joe.doe@example.com',
+                age: 25,
+                gender: 'male'
+              }
             }
           }
-        }
+        end.to change(User, :count).by(1)
 
         expect(response).to have_http_status(:created)
         expect(response.body).to include_json(
@@ -244,31 +271,15 @@ describe Api::UsersController do
           gender: 'male'
         )
       end
+    end
 
-      it 'returns conflict when type in request is incorrect' do
-        create(:organization)
-
-        post :create, params: {
-          data: {
-            type: 'invalid',
-            attributes: {
-              first_name: 'Joe',
-              last_name: 'Doe',
-              email: 'joe.doe@example.com',
-              age: 25,
-              gender: 'male'
-            }
-          }
-        }
-
-        expect(response).to have_http_status(:conflict)
-        expect(User.count).to eq 0
-      end
+    context 'when user is unauthorized' do
+      xit 'returns unauthorized'
     end
   end
 
   describe '#update' do
-    context 'authorized user' do
+    context 'when user is authorized' do
       it 'updates existing user' do
         user = create(
           :user,
@@ -311,7 +322,7 @@ describe Api::UsersController do
             }
           }
         )
-        expect(User.find(user.id)).to have_attributes(
+        expect(user.reload).to have_attributes(
           first_name: 'Alice',
           last_name: 'Kowalski',
           email: 'alice.kowalski@example.com',
@@ -320,68 +331,39 @@ describe Api::UsersController do
         )
       end
 
-      it 'returns conflict when type in request is incorrect' do
-        user = create(
-          :user,
-          first_name: 'Joe',
-          last_name: 'Doe',
-          email: 'joe.doe@example.com',
-          age: 50,
-          gender: 'male'
-        )
-
-        patch :update, params: {
-          id: user.id,
-          data: {
-            type: 'incorrect',
-            id: user.id,
-            attributes: {
-              first_name: 'Alice',
-              last_name: 'Kowalski',
-              email: 'alice.kowalski@example.com',
-              age: 30,
-              gender: 'female'
-            }
-          }
-        }
-
-        expect(response).to have_http_status(:conflict)
-        expect(User.find(user.id)).to have_attributes(
-          first_name: 'Joe',
-          last_name: 'Doe',
-          email: 'joe.doe@example.com',
-          age: 50,
-          gender: 'male'
-        )
-      end
-
-      it "returns not found when user with given id doesn't exist in database" do
-        patch :update, params: {
-          id: 1,
-          data: {
-            type: 'users',
+      context "when user with given id doesn't exist in the DB" do
+        it 'returns not found ' do
+          patch :update, params: {
             id: 1,
-            attributes: {
-              first_name: 'Alice',
-              last_name: 'Kowalski',
-              email: 'alice.kowalski@example.com',
-              age: 30,
-              gender: 'female'
+            data: {
+              type: 'users',
+              id: 1,
+              attributes: {
+                first_name: 'Alice',
+                last_name: 'Kowalski',
+                email: 'alice.kowalski@example.com',
+                age: 30,
+                gender: 'female'
+              }
             }
           }
-        }
 
-        expect(response).to have_http_status(:not_found)
-        expect(response.body).to include_json(
-          errors: [
-            {
-              status: 404,
-              code: 'Not found',
-              title: 'User not found'
-            }
-          ]
-        )
+          expect(response).to have_http_status(:not_found)
+          expect(response.body).to include_json(
+            errors: [
+              {
+                status: 404,
+                code: 'Not found',
+                title: 'User not found'
+              }
+            ]
+          )
+        end
       end
+    end
+
+    context 'when user is unauthorized' do
+      xit 'returns unauthorized'
     end
   end
 end
