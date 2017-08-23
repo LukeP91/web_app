83a7ca1dd66823e1a579ce459532d35e36a45818
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-23 15:26:39 +0200

    add authorization and authentication for json api

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,26 +1,26 @@
-class Api::UsersController < ApplicationController
-  class AuthorizationTokenMissing < StandardError; end
-
-  rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
-  rescue_from AuthorizationTokenMissing, with: :unauthenticated_user
-
-  before_action :authenticate_user_token!, only: [:index]
-
-
+class Api::UsersController < Api::ApiController
   def index
-    users = User.in_organization(@user.organization).order(:id).paginate(page: page, per_page: per_page)
+    users = User.in_organization(current_organization).order(:id).paginate(page: page, per_page: per_page)
+
+    authorize users
+
     render json: UsersSerializer.new(users, page, per_page).serialize, status: :ok
   end
 
   def show
-    user = User.find(params[:id])
+    user = User.in_organization(current_organization).find(params[:id])
+
+    authorize user
+
     render json: UserSerializer.new(user).serialize, status: :ok
   end
 
   def create
     user = User.new(user_params)
     user.password = user.password_confirmation = Devise.friendly_token.first(8)
-    user.organization = Organization.first
+    user.organization = current_organization
+
+    authorize user
 
     if user.save
       user.send_reset_password_instructions
@@ -31,7 +31,9 @@ class Api::UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
+    user = User.in_organization(current_organization).find(params[:id])
+
+    authorize user
 
     if user.update(user_params)
       render json: UserSerializer.new(user).serialize, status: :ok
@@ -41,7 +43,9 @@ class Api::UsersController < ApplicationController
   end
 
   def destroy
-    user = User.find(params[:id])
+    user = User.in_organization(current_organization).find(params[:id])
+
+    authorize user
 
     if user.delete
       render json: {}, status: :no_content
@@ -63,28 +67,4 @@ class Api::UsersController < ApplicationController
   def per_page
     params.fetch(:per_page, User.per_page).to_i
   end
-
-  def user_not_found
-    render json: { errors: [{ status: 404, code: 'Not found', title: 'User not found' }] }, status: :not_found
-  end
-
-  def authenticate_user_token!
-    payload = JsonWebToken.decode(authentication_token)
-    @user = User.find(payload[:id])
-  end
-
-  def authentication_token
-    token = request.headers['Authorization']
-
-    if token.nil?
-      raise AuthorizationTokenMissing
-    end
-
-    token.gsub('Bearer: ', '')
-  end
-
-  def unauthenticated_user
-    render json: { errors: [{ status: 401, code: 'Unauthorized', title: 'Unauthorized' }] }, status: :unauthorized
-  end
 end
-
