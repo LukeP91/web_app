c88ea9ec793d41bb6bdee4b659ff2a9aa6f49474
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-24 13:56:06 +0200

    add service which is responsible for sending welcome sms to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170808102550) do
+ActiveRecord::Schema.define(version: 20170824131902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170808102550) do
     t.integer  "age"
     t.boolean  "admin",                  default: false, null: false
     t.integer  "organization_id",        default: 1,     null: false
+    t.string   "mobile_phone"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
