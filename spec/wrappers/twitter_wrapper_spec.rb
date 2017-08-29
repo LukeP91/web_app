0662509fad94907a37a9a0a0283631b4ad17b5f6
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-29 14:02:41 +0200

    add tweet id from twitter

--- spec/wrappers/twitter_wrapper_spec.rb
+++ spec/wrappers/twitter_wrapper_spec.rb
@@ -8,13 +8,14 @@ describe TwitterWrapper do
       tweets = [tweet, tweet]
       allow(tweet).to receive(:full_text).and_return('First tweet #Ruby, #Rails', 'Second tweet #Ruby, #Rails')
       allow(tweet).to receive_message_chain('user.name').and_return('luke_pawlik')
+      allow(tweet).to receive(:id).and_return(1, 2)
       allow(client).to receive(:search).with('Ruby Rails -rt').and_return(tweets)
       allow(Twitter::REST::Client).to receive(:new).and_return(client)
 
       expect(TwitterWrapper.new.fetch(%w[Ruby Rails], 20)).to match_array(
         [
-          { user_name: 'luke_pawlik', message: 'First tweet #Ruby, #Rails', hashtags: %w[#Ruby #Rails] },
-          { user_name: 'luke_pawlik', message: 'Second tweet #Ruby, #Rails', hashtags: %w[#Ruby #Rails] }
+          { user_name: 'luke_pawlik', message: 'First tweet #Ruby, #Rails', hashtags: %w[#Ruby #Rails], tweet_id: 1 },
+          { user_name: 'luke_pawlik', message: 'Second tweet #Ruby, #Rails', hashtags: %w[#Ruby #Rails], tweet_id: 2 }
         ]
       )
     end
