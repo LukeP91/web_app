cc6c594ac8f50daee8406cd7e56643bfa8f07416
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-29 16:18:35 +0200

    inline one of doubles

--- spec/wrappers/twitter_wrapper_spec.rb
+++ spec/wrappers/twitter_wrapper_spec.rb
@@ -6,13 +6,12 @@ describe TwitterWrapper do
       Hashtag = Struct.new(:text)
       client = double('TwitterClient')
       tweet = double('Tweets')
-      tweets = [tweet, tweet]
       allow(tweet).to receive(:full_text).and_return('First tweet #Ruby, #Rails', 'Second tweet #Ruby, #Rails')
       allow(tweet).to receive_message_chain('user.name').and_return('luke_pawlik')
       allow(tweet).to receive(:id).and_return(1, 2)
       allow(tweet).to receive(:hashtags).and_return([Hashtag.new('Ruby'), Hashtag.new('Rails')])
       allow(client).to receive(:search).with('Ruby OR Rails -rt', count: 20, since_id: 1, result_type: 'recent')
-        .and_return(tweets)
+        .and_return([tweet, tweet])
       allow(Twitter::REST::Client).to receive(:new).and_return(client)
 
       expect(TwitterWrapper.new.fetch(%w[Ruby Rails], 1)).to match_array(
