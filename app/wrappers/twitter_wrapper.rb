eb5b1d8c9536e7478c8c77efc1b89e55d7821b6a
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-29 13:29:07 +0200

    add twitter wrapper

--- app/wrappers/twitter_wrapper.rb
+++ app/wrappers/twitter_wrapper.rb
@@ -1,4 +1,23 @@
 class TwitterWrapper
+  def initialize
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.consumer_key
+      config.consumer_secret     = Rails.application.secrets.consumer_secret
+      config.access_token        = Rails.application.secrets.access_token
+      config.access_token_secret = Rails.application.secrets.access_token_secret
+    end
+  end
+
   def fetch(hashtags, batch_size)
+    search_query = hashtags.join(' ') + ' -rt'
+    @client.search(search_query).map do |tweet|
+      { user_name: tweet.user.name, message: tweet.full_text, hashtags: tweet_hashtags(tweet) }
+    end
+  end
+
+  private
+
+  def tweet_hashtags(tweet)
+    tweet.full_text.scan(/#\w*/)
   end
 end
