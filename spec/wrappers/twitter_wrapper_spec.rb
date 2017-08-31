38534ee17cda6b396751aeff548bb1850583f00b
Lukasz Pawlik <l.pawlik@selleo.com>
2017-08-31 09:53:27 +0200

    refactor twitter wrapper tests to use vcr

--- spec/wrappers/twitter_wrapper_spec.rb
+++ spec/wrappers/twitter_wrapper_spec.rb
@@ -3,24 +3,20 @@ require 'rails_helper'
 describe TwitterWrapper do
   describe '#fetch' do
     it 'returns array of tweets' do
-      Hashtag = Struct.new(:text)
-      client = double('TwitterClient')
-      tweet = double('Tweets')
-      allow(tweet).to receive(:full_text).and_return('First tweet #Ruby, #Rails', 'Second tweet #Ruby, #Rails')
-      allow(tweet).to receive_message_chain('user.name').and_return('luke_pawlik')
-      allow(tweet).to receive(:id).and_return(1, 2)
-      allow(tweet).to receive(:hashtags).and_return([Hashtag.new('Ruby'), Hashtag.new('Rails')])
-      allow(client).to receive(:search).with('Ruby OR Rails -rt', count: 20, since_id: 1, result_type: 'recent')
-        .and_return([tweet, tweet])
-      allow(Twitter::REST::Client).to receive(:new).and_return(client)
+      VCR.use_cassette 'twitter_search_response' do
+        expect(TwitterWrapper.new.fetch(%w[#Ruby #Rails], 1)).to include(
+          user_name: 'Rails Links',
+          message: 'Announcing the Modularized AWS SDK for Ruby (Version 3) | Amazon Web Services #ruby #rails https://t.co/M9qoPT0sxd',
+          hashtags: %w[ruby rails],
+          tweet_id: 903150378962825216
+        )
+      end
+    end
 
-      expect(TwitterWrapper.new.fetch(%w[Ruby Rails], 1)).to match_array(
-        [
-          { user_name: 'luke_pawlik', message: 'First tweet #Ruby, #Rails', hashtags: %w[Ruby Rails], tweet_id: 1 },
-          { user_name: 'luke_pawlik', message: 'Second tweet #Ruby, #Rails', hashtags: %w[Ruby Rails], tweet_id: 2 }
-        ]
-      )
+    it 'returns only 20 tweets at once' do
+      VCR.use_cassette 'twitter_search_response' do
+        expect(TwitterWrapper.new.fetch(%w[#Ruby #Rails], 1).count).to eq(20)
+      end
     end
   end
 end
-
