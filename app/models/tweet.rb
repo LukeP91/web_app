426c20372cea10d257c3d1d6df455ed70ad63b1f
Lukasz Pawlik <l.pawlik@selleo.com>
2017-09-01 13:46:22 +0200

    return sorted array with most common words

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,4 +7,29 @@ class Tweet < ApplicationRecord
   validates :user_name, :message, :tweet_id, presence: true
 
   scope :in_organization, ->(organization) { joins(:sources).where(sources: { organization_id: organization.id }) }
+
+  def self.most_common_words(organization)
+    Tweet.in_organization(organization)
+      .map(&:message)
+      .map { |message| message_words_count(message) }
+      .inject({}) do |count, message_count|
+        count.merge(message_count) { |_key, count_value, message_count_value| count_value + message_count_value }
+      end
+      .sort_by { |word, count| count }
+      .reverse
+  end
+
+  private
+
+  def self.message_words_count(message)
+    words_count = {}
+    message.split(' ').each do |word|
+      if words_count[word].present?
+        words_count[word] += 1
+      else
+        words_count[word] = 1
+      end
+    end
+    words_count
+  end
 end
