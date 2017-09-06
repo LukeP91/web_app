6fdfe91174697863fb6439a8274907f9bc7aad7c
Lukasz Pawlik <l.pawlik@selleo.com>
2017-09-06 14:39:51 +0200

    remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require turbolinks
 //= require highcharts
 //= require highcharts/modules/exporting
 //= require_tree .
