5bf6a037534b90a5191d0caa8c91c7e94f4c446c
Lukasz Pawlik <l.pawlik@selleo.com>
2017-09-13 10:20:41 +0200

    add pie charts to both statistics

---
+++ app/assets/javascripts/most_common_words_pie_chart.js
@@ -0,0 +1,41 @@
+$(document).ready(() => {
+  const $container = $('<div>').appendTo($('#most_common_words'));
+  const totalWords = $('#most_common_words').data('wordsStats').map((wordStats) => wordStats.count)
+    .reduce((accu, current) => accu + current);
+  const wordsStats = $('#most_common_words').data('wordsStats')
+    .map((wordStats) => ({ name: wordStats.word, y: +(wordStats.count * 100 / totalWords).toFixed(2) }));
+
+  window.chart = new Highcharts.Chart({
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie',
+        renderTo: $container[0]
+    },
+    title: {
+      text: 'Most common words count',
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    credits: {
+      enabled: false,
+    },
+    series: [{
+        name: 'Words',
+        colorByPoint: true,
+        data: wordsStats
+    }]
+  });
+});
