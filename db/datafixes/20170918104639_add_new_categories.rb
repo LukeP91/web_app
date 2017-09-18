ff3053bfa7d1f0b7fb15a0cb09ffc0d9e9e81c30
Lukasz Pawlik <l.pawlik@selleo.com>
2017-09-18 11:29:44 +0200

    apply feedback

--- db/datafixes/20170918104639_add_new_categories.rb
+++ db/datafixes/20170918104639_add_new_categories.rb
@@ -1,10 +1,8 @@
 class Datafixes::AddNewCategories < Datafix
   def self.up
     organization = Organization.find_by(name: 'Google')
-    Category.find_or_create_by(name: 'school', organization: organization)
-    Category.find_or_create_by(name: 'travel', organization: organization)
-    Category.find_or_create_by(name: 'people', organization: organization)
-    Category.find_or_create_by(name: 'living', organization: organization)
-    Category.find_or_create_by(name: 'nonprofit', organization: organization)
+    %w[school travel people living nonprofit].each do |category_name|
+      Category.find_or_create_by(name: category_name, organization: organization)
+    end
   end
 end
